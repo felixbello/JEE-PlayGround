3fec13e1e5aa078c739e76417ca0ae8ba78b2ffe
felix <felix.bello@ch.zuken.com>
2020-04-10 10:33:11 +0200

    running version

--- src/main/java/com/zuken/jax/DevelopersResource.java
+++ src/main/java/com/zuken/jax/DevelopersResource.java
@@ -1,13 +1,22 @@
 package com.zuken.jax;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("developers")
 public class DevelopersResource {
 
     @GET
-    public String developer(){
+    public String developers(){
         return "duchess,duke";
+    }
+
+    @GET
+    @Path("{first}-{last}")
+    public String developer(@PathParam("first") String first, @PathParam("last") String last){
+        return first + "_" + last;
     }
 }
