184925edc5bbbba4a99f834283799a7a5df1c800
felix <felix.bello@ch.zuken.com>
2020-04-10 13:39:25 +0200

    running well

---
+++ src/main/java/com/zuken/jax/Developer.java
@@ -0,0 +1,23 @@
+package com.zuken.jax;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement
+public class Developer {
+
+    @XmlElement(name="firstname")
+    private String name;
+    private String last;
+
+    public Developer() {
+    }
+
+    public Developer(String name, String last) {
+        this.name = name;
+        this.last = last;
+    }
+}
--- src/main/java/com/zuken/jax/DevelopersResource.java
+++ src/main/java/com/zuken/jax/DevelopersResource.java
@@ -2,21 +2,27 @@ package com.zuken.jax;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 
 @Path("developers")
 public class DevelopersResource {
 
     @GET
-    public String developers(){
-        return "duchess,duke";
+    public String developers(@Context HttpHeaders http){
+//        return "duchess, duke";
+        return "duchess, duke";
     }
 
     @GET
     @Path("{first}-{last}")
-    public String developer(@PathParam("first") String first, @PathParam("last") String last){
-        return first + "_" + last;
+    public Developer developer(@PathParam("first") String first, @PathParam("last") String last){
+        return new Developer(first,last);
     }
 }
