92a7c1b4bd32a88c144d738454b7ef1d004bc743
Felix Bello <felix@cymac.local>
2021-01-24 02:15:14 +0100

    check status of an instance

--- src/main/java/com/zuken/jax/InstanceResource.java
+++ src/main/java/com/zuken/jax/InstanceResource.java
@@ -1,15 +1,72 @@
 package com.zuken.jax;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.compute.Compute;
+import com.google.api.services.compute.model.Instance;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @Path( "instance" )
 public class InstanceResource {
 
+    private static HttpTransport httpTransport;
+    private static final String APPLICATION_NAME = "";
+    /** Set PROJECT_ID to your Project ID from the Overview pane in the Developers console. */
+    private static final String PROJECT_ID = "online-school-labs";
+
+    /** Set Compute Engine zone. */
+    private static final String ZONE_NAME = "europe-west6-a";
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
     @GET
     public String developer(@Context HttpHeaders http){
         return "Instance Servlet Up and Running";
+    }
+
+    @GET
+    @Path("{name}")
+    public String developer(@PathParam("name") String name) {
+
+        String found = null;
+
+        try {
+
+            GoogleCredentials credentials = Helper.getGoogleCredentials();
+            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
+
+            // Create Compute Engine object for listing instances.
+            Compute compute =
+                    new Compute.Builder(httpTransport, JSON_FACTORY, requestInitializer)
+                            .setApplicationName(APPLICATION_NAME)
+                            .build();
+
+            Compute.Instances.Get request = compute.instances().get(PROJECT_ID, ZONE_NAME, name);
+
+            Instance response = request.execute();
+
+            found = response.getStatus();
+
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return found;
     }
 }
