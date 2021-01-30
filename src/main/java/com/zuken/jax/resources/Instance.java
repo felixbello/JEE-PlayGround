6e955300d86d739422ac0f2ac6f457ffce723e02
felix <felix.bello@becit.ch>
2021-01-30 21:07:49 +0100

    just trying

--- src/main/java/com/zuken/jax/resources/Instance.java
+++ src/main/java/com/zuken/jax/resources/Instance.java
@@ -6,6 +6,9 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.compute.Compute;
+import com.google.api.services.compute.model.AccessConfig;
+import com.google.api.services.compute.model.AttachedDisk;
+import com.google.api.services.compute.model.NetworkInterface;
 import com.google.api.services.compute.model.Operation;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
@@ -21,8 +24,11 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 @Path( "instance" )
 public class Instance {
@@ -176,6 +182,88 @@ public class Instance {
         return found;
     }
 
+    public String create(@PathParam( "zone" ) String zone, String name){
+        String created = null;
 
+        try {
+            GoogleCredentials credentials = Helper.getGoogleCredentials();
+            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
+
+            // Create Compute Engine object for listing instances.
+            Compute compute =
+                    new Compute.Builder(httpTransport, JSON_FACTORY, requestInitializer)
+                            .setApplicationName(APPLICATION_NAME)
+                            .build();
+        }
+    }
+
+    private static void createInstance(Compute computeService) throws IOException, InterruptedException {
+
+        boolean diskCreated = false;
+        String zone = "europe-west6-a";
+        String region = null;
+
+        for (int i = 0; i < 6; i++) {
+            if (regions == null){
+                regions = getRegionsList( computeService );
+            }
+
+            region = regions.get( "GERMANY" );
+
+            zonList = getZonesListFromRegion( computeService, region );
+
+            zone = selectZone(zonList);
+
+            createDisk( computeService, PROJECT_ID, zone, "app-" + i );
+
+            while (!(diskCreated)) {
+                diskCreated = checkStatus( computeService, PROJECT_ID, zone, "app-" + i );
+                System.out.println( "still not ready... wait" );
+                TimeUnit.SECONDS.sleep( 3 );
+            }
+
+
+            com.google.api.services.compute.model.Instance requestInstanceBody = new com.google.api.services.compute.model.Instance();
+            requestInstanceBody.setName( "testing-app-" + i );
+//            requestInstanceBody.setMachineType( "projects/online-school-labs/zones/europe-west6-a/machineTypes/e2-medium" );
+            requestInstanceBody.setMachineType( "projects/online-school-labs/zones/"+zone+"/machineTypes/e2-standard-8" );
+            requestInstanceBody.setZone( "projects/online-school-labs/zones/" + zone );
+
+            List<NetworkInterface> netWorkInterfacesList = new ArrayList<>();
+            NetworkInterface networkInterface = new NetworkInterface();
+            networkInterface.setKind( "compute#networkInterface" );
+
+            String ipAddress = createFixedIP( computeService, region, "testing-app-" + i );
+            List<AccessConfig> accessConfigList = new ArrayList<>();
+            AccessConfig accessConfig = new AccessConfig();
+            accessConfig.setKind( "compute#accessConfig" );
+            accessConfig.setName( "External NAT" );
+            accessConfig.setType( "ONE_TO_ONE_NAT" );
+            accessConfig.setNatIP( ipAddress );
+            accessConfigList.add( accessConfig );
+
+            networkInterface.setAccessConfigs( accessConfigList );
+
+            netWorkInterfacesList.add( networkInterface );
+
+            requestInstanceBody.setNetworkInterfaces( netWorkInterfacesList );
+
+            // Disk
+            List<AttachedDisk> diskList = new ArrayList<>();
+            AttachedDisk bootD = new AttachedDisk();
+            bootD.setBoot( true );
+            bootD.setSource( "projects/online-school-labs/zones/" + zone + "/disks/" + "app-" + i );
+            bootD.setAutoDelete( true );
+            diskList.add( bootD );
+
+            requestInstanceBody.setDisks( diskList );
+
+            Compute.Instances.Insert instInsert = computeService.instances().insert( PROJECT_ID, zone, requestInstanceBody );
+            Operation reqInst = instInsert.execute();
+
+            diskCreated = false;
+        }
+    }
 
 }
